86285912a7ec65223cfd44b7eb097e2c91b1e427
sukritdutta234 <artemisofficial10@gmail.com>
2026-01-05 14:31:34 +0530

    Fix profit/loss calculation and syntax errors

--- profitloss.c
+++ profitloss.c
@@ -6,21 +6,21 @@ int main(){
     printf("Enter the marker price:");
     scanf("%f",&marker_price);
     printf("Enter the discount:");
-    scanf("%f%%",&discount);
+    scanf("%f",&discount);
     selling_price = marker_price - (marker_price*discount/100);
     if(buying_price<selling_price){
         difference = selling_price - buying_price;
-        result_percentage = (difference/marker_price)*100;
+        result_percentage = (difference/buying_price)*100;
         printf("Seller made a profit of %.2f%%.\n",result_percentage);
     }
-    if else(buying_price>selling_price){
+    else if(buying_price>selling_price){
         difference = buying_price - selling_price;
-        result_percentage = (difference/marker_price)*100;
+        result_percentage = (difference/buying_price)*100;
         printf("Seller made a loss of %.2f%%.\n", result_percentage);
     }
     else{
         // buying_price == selling_price
-        printf("Seller made no profit and no loss.\n")
+        printf("Seller made no profit and no loss.\n");
     }
 
     return 0;
